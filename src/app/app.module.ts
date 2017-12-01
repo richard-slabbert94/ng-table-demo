91b3b1fa713b8640adecf78001f3334994bca9d9
richard-slabbert94 <richard.slabbert94@gmail.com>
2017-12-01 16:56:35 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [NgxDatatableModule, BrowserModule],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
